7cf5ed598af859138fdf6d2c077577464df9192e
Jeroen Haegebaert <Jeroen.haegebaert@ae.be>
2017-12-01 22:11:32 +0100

    My take in C#

---
+++ day 01/Jeroen - C#/Captcha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Captcha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var sw = Stopwatch.StartNew();
+            var result1 = Captcha.Calculate(input, 1);
+            var result2 = Captcha.Calculate(input, input.Length/2);
+            var elapsed = sw.Elapsed;
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(elapsed);
+        }
+    }
+
+
+}
