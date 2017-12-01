63789099da422c72b176a780b3fa156f3c7187c0
Jeroen Haegebaert <Jeroen.haegebaert@ae.be>
2017-12-01 22:20:11 +0100

    minor cleanup

--- day 01/Jeroen - C#/Captcha/Captcha.cs
+++ day 01/Jeroen - C#/Captcha/Captcha.cs
@@ -2,39 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Captcha
+class Captcha
 {
-    class Captcha
+    public static int Calculate(string input, int lookahead)
     {
-        public static int Calculate(string input, int lookahead)
-        {
-            return GetCandidates(input, lookahead).Sum();
-        }
-
-        private static IEnumerable<int> GetCandidates(string input, int lookahead)
-        {
-            var enumerator1 = WrapAround(input, lookahead).GetEnumerator();
-            var enumerator2 = WrapAround(input, lookahead).Skip(lookahead).GetEnumerator();
-            if (!enumerator1.MoveNext()) yield break;
-            if (!enumerator2.MoveNext()) yield break;
-            do
-            {
-                int first = enumerator1.Current;
-                int second = enumerator2.Current;
-                if (first == second) yield return first - '0';
-            } while (enumerator1.MoveNext() && enumerator2.MoveNext());
-
-
-        }
-
-        static IEnumerable<char> WrapAround(string input, int n)
-        {
-            for (int i = 0; i < input.Length; i++)
-                yield return input[i];
-            for (int i = 0; i < input.Length && i < n; i++)
-                yield return input[i];
-        }
+        return GetCandidates(input, lookahead).Sum();
     }
 
+    private static IEnumerable<int> GetCandidates(string input, int lookahead)
+    {
+        var enumerator1 = WrapAround(input, lookahead).GetEnumerator();
+        var enumerator2 = WrapAround(input, lookahead).Skip(lookahead).GetEnumerator();
+        if (!enumerator1.MoveNext()) yield break;
+        if (!enumerator2.MoveNext()) yield break;
+        do
+        {
+            int first = enumerator1.Current;
+            int second = enumerator2.Current;
+            if (first == second) yield return first - '0';
+        } while (enumerator1.MoveNext() && enumerator2.MoveNext());
 
+
+    }
+
+    static IEnumerable<char> WrapAround(string input, int n)
+    {
+        for (int i = 0; i < input.Length; i++)
+            yield return input[i];
+        for (int i = 0; i < input.Length && i < n; i++)
+            yield return input[i];
+    }
 }
--- day 01/Jeroen - C#/Captcha/Program.cs
+++ day 01/Jeroen - C#/Captcha/Program.cs
@@ -2,24 +2,19 @@
 using System.Diagnostics;
 using System.IO;
 
-namespace Captcha
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var input = File.ReadAllText("input.txt");
+        var input = File.ReadAllText("input.txt");
 
-            var sw = Stopwatch.StartNew();
-            var result1 = Captcha.Calculate(input, 1);
-            var result2 = Captcha.Calculate(input, input.Length/2);
-            var elapsed = sw.Elapsed;
+        var sw = Stopwatch.StartNew();
+        var result1 = Captcha.Calculate(input, 1);
+        var result2 = Captcha.Calculate(input, input.Length / 2);
+        var elapsed = sw.Elapsed;
 
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
-            Console.WriteLine(elapsed);
-        }
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+        Console.WriteLine(elapsed);
     }
-
-
 }
