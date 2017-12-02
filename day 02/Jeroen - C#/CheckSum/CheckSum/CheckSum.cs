6b181a777f9e282de2d01448a7a919c8930cf6c4
Jeroen Haegebaert <jeroen@haegebaert.com>
2017-12-02 14:04:51 +0100

    linq ftw

---
+++ day 02/Jeroen - C#/CheckSum/CheckSum/CheckSum.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+static class CheckSum
+{
+    public static int CheckSum1(TextReader reader) => ReadInts(reader)
+        .Select(LargestDifference)
+        .Sum();
+
+    public static int CheckSum2(TextReader reader) => ReadInts(reader)
+        .Select(EvenDivision)
+        .Sum();
+
+    static int LargestDifference(IEnumerable<int> input)
+    {
+        var aggregate = input.Aggregate(
+            (min: int.MaxValue, max: int.MinValue),
+            (x, n) => (n < x.min ? n : x.min, n > x.max ? n : x.max)
+        );
+        return aggregate.max - aggregate.min;
+    }
+    static int EvenDivision(IEnumerable<int> input)
+    {
+        var ints = input.OrderByDescending(i => i).ToArray();
+        return ( 
+            from x in ints.Select((value, index) => (idx: index, value: value))
+            from y in ints.Skip(x.idx + 1)
+            where (decimal) x.value % y == 0
+            select x.value / y
+            ).FirstOrDefault();
+    }
+
+    public static IEnumerable<IEnumerable<int>> ReadInts(TextReader reader) 
+        => ReadLines(reader).Select(line => line.Split('\t').Select(int.Parse));
+
+    private static IEnumerable<string> ReadLines(TextReader reader)
+    {
+        while (reader.Peek() >= 0) yield return reader.ReadLine();
+    }
+}
---
+++ day 02/Jeroen - C#/CheckSum/CheckSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var reader = new StreamReader(File.OpenRead("input.txt")))
+        {
+            Console.WriteLine(CheckSum.CheckSum1(reader));
+        }
+        using (var reader = new StreamReader(File.OpenRead("input.txt")))
+        {
+            Console.WriteLine(CheckSum.CheckSum2(reader));
+        }
+    }
+}
