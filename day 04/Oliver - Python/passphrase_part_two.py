0fd992c941886905b2f6b15ccdc038c148949ec2
OliverBLMS <oliver.belmans@gmail.com>
2017-12-05 10:36:41 +0100

    solution day 4

---
+++ day 04/Oliver - Python/passphrase_part_two.py
@@ -0,0 +1,42 @@
+"""
+--- Part Two ---
+
+For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+For example:
+
+abcde fghij is a valid passphrase.
+abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+iiii oiii ooii oooi oooo is valid.
+oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+Under this new system policy, how many passphrases are valid?
+
+"""
+import itertools
+
+def read_input(path):
+    with open(path, "r") as a:
+        return [line.split() for line in a]
+
+passphrases = read_input("day 04/Oliver - Python/input.txt")
+
+def compare(phrases):
+    result = []
+    for line in phrases:
+        temp = []
+        for word1, word2 in itertools.combinations(line, 2):
+            temp.append((sorted(word1) == sorted(word2)))
+        result.append(not(max(temp)))
+    return result
+
+# Test example
+test = read_input("day 04/Oliver - Python/test.txt")
+compare(test)
+
+# On input.txt
+result = compare(passphrases)
+len(result)
+# Valid results
+print(sum(result))
+
