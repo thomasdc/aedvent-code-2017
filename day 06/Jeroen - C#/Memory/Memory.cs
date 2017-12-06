851acfcd431a92a0d50f0f0e5b8dc3cdc271c170
Jeroen Haegebaert <jeroen@haegebaert.com>
2017-12-06 16:16:08 +0100

    day 6 in c#

---
+++ day 06/Jeroen - C#/Memory/Memory.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Memory
+{
+    static class Memory
+    {
+        public static (int steps, int loopSize) Cycles(ImmutableArray<byte> input)
+        {
+            var list = new List<ImmutableArray<byte>>();
+            int steps = 0;
+            int max = 0;
+            var cycle = input;
+            while (true)
+            {
+                var m = cycle.FindMax();
+                if (m.value > max) max = m.value;
+                steps++;
+                cycle = DoOneCycle(cycle);
+
+                var index = list.FindIndex(x => x.SequenceEqual(cycle));
+                if (index >= 0)
+                {
+                    Console.WriteLine(max);
+                    return (steps, list.Count - index);
+                }
+
+                list.Add(cycle);
+            };
+
+        }
+        public static (int steps, int loopSize) MutableCycles(ImmutableArray<byte> input)
+        {
+            var list = new List<ImmutableArray<byte>>();
+            int steps = 0;
+            var cycle = input;
+            int max = 0;
+            while (true)
+            {
+                var m = cycle.FindMax();
+                if (m.value > max) max = m.value;
+                steps++;
+                cycle = DoOneCycle(cycle);
+                var index = list.FindIndex(x => x.SequenceEqual(cycle));
+                if (index >= 0)
+                {
+                    Console.WriteLine(max);
+                    return (steps, list.Count - index);
+                }
+
+                list.Add(cycle);
+            };
+        }
+
+        public static (int index, int value) FindMax(this IEnumerable<byte> input) =>
+            input
+                .Select((value, index) => (index: index, value: value))
+                .Aggregate((index: -1, value: int.MinValue), (x, y) => y.value > x.value ? y : x);
+
+        static void DoOneCycle(byte[] input)
+        {
+            var max = input.FindMax();
+            var length = input.Length;
+            var quotient = max.value / length;
+            var remainder = max.value % length;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var value = input[i];
+                var j = (i + length - max.index - 1) % length;
+                var term1 = i == max.index ? 0 : value;
+                var term2 = quotient;
+                var term3 = j < remainder ? 1 : 0;
+                input[i] = (byte)(term1 + term2 + term3);
+            }
+        }
+        public static ImmutableArray<byte> DoOneCycle(ImmutableArray<byte> input)
+        {
+            var max = input.FindMax();
+            var length = input.Length;
+            var quotient = max.value / length;
+            var remainder = max.value % length;
+
+            // 1 2 6 3  
+            // 3 3 1 5  
+            // max = (2,6)
+            // length = 4
+            // quotient = 1
+            // remainder = 2
+
+            // 0 1 2 3  i
+            // F F T F  i == max.index ?
+            //*1 2 0 3  0 : value
+            //*1 1 1 1  
+            // 1 2 3 0  j = (i + length - max.index - 1) % length
+            // T F F T  j < remainder ? 
+            //*1 0 0 1  
+            //*3 3 1 5
+
+
+            var q =
+                from t in input.Select((value, i) => (value: value, i: i))
+                let i = t.i
+                let value = t.value
+                let j = (i + length - max.index - 1) % length
+                let term1 = i == max.index ? 0 : value
+                let term2 = quotient
+                let term3 = j < remainder ? 1 : 0
+                select (term1, term2, term3);
+
+            var redistributed = q.Select(x => (byte)(x.Item1 + x.Item2 + x.Item3)).ToImmutableArray();
+            return redistributed;
+        }
+
+
+    }
+}
---
+++ day 06/Jeroen - C#/Memory/Tests.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Immutable;
+using Xunit;
+
+namespace Memory
+{
+    public class Tests
+    {
+        [Fact]
+        public void CycleTest_WhenLargestValueIsEvenlyDivisable()
+        {
+            Assert.Equal(new byte[] { 2, 2, 2, 2 }, Memory.DoOneCycle(new byte[] { 0, 0, 0, 8 }.ToImmutableArray()));
+        }
+        [Fact]
+        public void CycleTest_WhenLargestValueIsNotEvenlyDivisable()
+        {
+            Assert.Equal(new byte[] { 2, 2, 2, 1 }, Memory.DoOneCycle(new byte[] { 0, 0, 0, 7 }.ToImmutableArray()));
+        }
+        [Fact]
+        public void CycleTest_WhenLargestValueIsNotEvenlyDivisable_AndMaxItemNotLast()
+        {
+            Assert.Equal(new byte[] { 2, 2, 1, 2 }, Memory.DoOneCycle(new byte[] { 0, 0, 7, 0 }.ToImmutableArray()));
+        }
+        [Fact]
+        public void CycleTest_Step1()
+        {
+            Assert.Equal(new byte[] { 2, 4, 1, 2 }, Memory.DoOneCycle(new byte[] { 0, 2, 7, 0 }.ToImmutableArray()));
+        }
+        [Fact]
+        public void CycleTest_Step2()
+        {
+            Assert.Equal(new byte[] { 3, 1, 2, 3 }, Memory.DoOneCycle(new byte[] { 2, 4, 1, 2 }.ToImmutableArray()));
+        }
+        [Fact]
+        public void CycleTest_Step3()
+        {
+            Assert.Equal(new byte[] { 0, 2, 3, 4 }, Memory.DoOneCycle(new byte[] { 3, 1, 2, 3 }.ToImmutableArray()));
+        }
+        [Fact]
+        public void CycleTest_Step4()
+        {
+            Assert.Equal(new byte[] { 1, 3, 4, 1 }, Memory.DoOneCycle(new byte[] { 0, 2, 3, 4 }.ToImmutableArray()));
+        }
+        [Fact]
+        public void CycleTest_Step5()
+        {
+            Assert.Equal(new byte[] { 2, 4, 1, 2 }, Memory.DoOneCycle(new byte[] { 1, 3, 4, 1 }.ToImmutableArray()));
+        }
+
+        [Theory]
+        [InlineData(5, 4, 0, 2, 7, 0)]
+        public void Example(int expectedSteps, int expectedLoopSize, params byte[] input)
+        {
+            var result = Memory.Cycles(input.ToImmutableArray());
+            Assert.Equal(expectedSteps, result.steps);
+            Assert.Equal(expectedLoopSize, result.loopSize);
+        }
+
+        [Fact]
+        public void FindMax_FindsLargestElement()
+        {
+            var input = new byte[] { 0, 2, 1 };
+            var max = input.FindMax();
+            Assert.Equal((1, 2), max);
+        }
+
+        [Fact]
+        public void FindMax_TiesWonByLowestIndex()
+        {
+            var input = new byte[] { 0, 0 };
+            var max = input.FindMax();
+            Assert.Equal(0, max.index);
+        }
+    }
+}
