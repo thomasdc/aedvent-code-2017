33c4b709bb3df99756301afb0c0cfc911e09e505
Kevin Stobbelaar <kevin.stobbelaar@UGent.be>
2017-12-07 16:56:41 +0100

    Finished d02p01

---
+++ day 02/Kevin S - Haskell/checksum.hs
@@ -0,0 +1,10 @@
+import Data.List.Split (splitOn)
+import System.IO
+
+calculateDiff :: [Int] -> Int
+calculateDiff xs = (maximum xs) - (minimum xs)
+
+main = do
+  contents <- readFile "input.txt"
+  let contentsParsed = map (map (read :: String -> Int)) . map words $ lines contents 
+  (print . sum . map calculateDiff) $ contentsParsed
