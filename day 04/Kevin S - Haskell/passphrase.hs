be22eda12a8223adaa281eb7d9b493a4a60c0076
Kevin Stobbelaar <kevin.stobbelaar@UGent.be>
2017-12-08 20:10:58 +0100

    Finished d04p02

--- day 04/Kevin S - Haskell/passphrase.hs
+++ day 04/Kevin S - Haskell/passphrase.hs
@@ -1,10 +1,7 @@
 import System.IO
-import System.IO
 import Data.List
-
-hasDuplicates :: [String] -> Bool
-hasDuplicates xs = nub xs == xs 
 
 main = do
   contents <- readFile "input.txt"
-  print . length . filter hasDuplicates . map words $ lines contents
+  print . length . filter (\xs -> nub xs == xs) . map words $ lines contents
+  print . length . filter (\xs -> nub xs == xs) . map (map sort) . map words $ lines contents
