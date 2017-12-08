02332f954ecefe7160e673805695a2e27a6626ea
ThomasDC <Thomas.DeCraemer@ravago.com>
2017-12-08 20:56:19 +0100

    day 6 part 1 c#

---
+++ day 06/Thomas - C#/Memory/Memory/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Memory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt").Split('\t').Select(int.Parse).ToArray();
+            Console.WriteLine(Part1(input));
+        }
+
+        public static int Part1(int[] banks)
+        {
+            var cycleIndex = 0;
+            var blockDistributions = new HashSet<string>();
+
+            do
+            {
+                blockDistributions.Add(string.Join("", banks));
+                var maxIndex = Array.IndexOf(banks, banks.Max());
+                var numberOfBlocksToRedistribute = banks[maxIndex];
+                banks[maxIndex] = 0;
+                for (var i = 1; i <= numberOfBlocksToRedistribute; i++)
+                {
+                    banks[(maxIndex + i) % banks.Length]++;
+                }
+                cycleIndex++;
+            } while (!blockDistributions.Contains(string.Join("", banks)));
+            
+            return cycleIndex;
+        }
+    }
+}
