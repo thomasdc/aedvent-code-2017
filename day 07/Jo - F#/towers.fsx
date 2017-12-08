8b1bb34471ad7d2d432d420a59c330d0c54109d2
Jo Van Eyck <jo.vaneyck@ae.be>
2017-12-08 17:18:24 +0100

    catching up on day 7 part 2

--- day 07/Jo - F#/towers.fsx
+++ day 07/Jo - F#/towers.fsx
@@ -1,5 +1,6 @@
 #r @"..\..\dependencies\Jo\.paket\packages\Unquote\lib\net45\Unquote.dll"
 open Swensen.Unquote
+open System.Windows.Forms.VisualStyles.VisualStyleElement.ListView
 
 let splitLines (text : string) = text.Split('\n')
 
@@ -65,13 +66,17 @@ type Tower = { name : string; directlyAbove : Tower list}
 let buildTree rootName (map : AboveMap) =
     let rec buildRec rootName =
         let aboves = map |> Seq.filter (fun (bel, abo) -> bel.name = rootName) |> Seq.map snd
-        { name = rootName; directlyAbove = aboves |> Seq.map buildRec}
+        { name = rootName; directlyAbove = aboves |> Seq.map buildRec |> Seq.toList}
     
     buildRec rootName
 
-let map = input |> parse |> buildAboveMap
-let root = "vtzay"//findRoot map
-buildTree root map
+type WeightedTree = { name : string; weight : int; above : WeightedTree list}
+
+let rec weigh (programs : Program seq) (tower : Tower) =
+    let programWeight = (programs |> Seq.find (fun p ->  p.name = tower.name)).weight
+    let weightedSubtrees = tower.directlyAbove |> List.map (weigh programs)
+    let w = programWeight + (weightedSubtrees |> Seq.fold (fun acc t -> acc + t.weight) 0)
+    { name = tower.name; above = weightedSubtrees; weight = w }
 
 printf "Testing..."
 test <@ parseLine "test (33)" = {name = "test"; weight = 33; immediatelyAbove = [] } @>
@@ -79,4 +84,26 @@ test <@ (parseLine "n (1) -> a, b, cd").immediatelyAbove = ["a";"b";"cd"] @>
 test <@ (solve example).name = "tknk" @>
 printfn "..done"
 
+//part 1
 //solve input //vtzay
+
+//part 2
+let parsed = input |> parse 
+let map = parsed |> buildAboveMap
+let root = "vtzay"//findRoot map
+let tree = buildTree root map
+let weightedTree = weigh parsed tree
+
+let rec walk (tree : WeightedTree) =
+    let subtreeweights = tree.above |> Seq.groupBy (fun t -> t.weight)
+    if subtreeweights |> Seq.length <> 1 then
+        let different = subtreeweights |> Seq.find (fun (_, group) -> group |> Seq.length = 1) |> snd |> Seq.head
+        let otherWeights = subtreeweights |> Seq.find (fun (_, group) -> group |> Seq.length > 1) |> fst
+        printfn "Node %A is unbalanced, walking down its different child %A. Other subtrees have weight %A" (tree.name, tree.weight) (different.name, different.weight) otherWeights
+        walk different
+    else
+        printfn "Everything from here on upward is balanced"
+
+walk weightedTree
+//lnpuarm needs to slim down 1437 - 1429 = 8
+let realWeight = parsed |> Seq.find (fun p -> p.name = "lnpuarm") |> (fun p -> p.weight)
