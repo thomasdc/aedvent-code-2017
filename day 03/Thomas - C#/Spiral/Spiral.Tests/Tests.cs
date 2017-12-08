a11e22f435eb0ab27eb1d6d13c21fb3b6060b125
ThomasDC <Thomas.DeCraemer@ravago.com>
2017-12-08 09:56:36 +0100

    day 3 part 2 c#

--- day 03/Thomas - C#/Spiral/Spiral.Tests/Tests.cs
+++ day 03/Thomas - C#/Spiral/Spiral.Tests/Tests.cs
@@ -14,5 +14,16 @@ namespace Spiral.Tests
             var actual = Program.Part1(input);
             Assert.Equal(expected, actual);
         }
+        
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(5, 10)]
+        [InlineData(26, 54)]
+        [InlineData(655, 747)]
+        public void TestPart2(int input, int expected)
+        {
+            var actual = Program.Part2(input);
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- day 03/Thomas - C#/Spiral/Spiral/Program.cs
+++ day 03/Thomas - C#/Spiral/Spiral/Program.cs
@@ -8,12 +8,42 @@ namespace Spiral
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Part1(265149));
+            Console.WriteLine(Part2(265149));
         }
 
         public static int Part1(int input)
         {
             return Cells().Skip(input-1).Select(_ => Math.Abs(_.column) + Math.Abs(_.row)).First();
+        }
+
+        public static int Part2(int input)
+        {
+            var grid = new Grid();
+            grid.Set(0, 0, 1);
+            foreach (var cell in Cells().Skip(1))
+            {
+                var sum = GetSumOfAdjacentCells(grid, cell);
+                grid.Set(cell.column, cell.row, sum);
+                if (sum > input)
+                {
+                    return sum;
+                }
+            }
+            
+            return -1;
+        }
+
+        private static int GetSumOfAdjacentCells(Grid grid, (int row, int column) cell)
+        {
+            var sum = 0;
+            for (var i = -1; i <= 1; i++)
+            {
+                for (var j = -1; j <= 1; j++)
+                {
+                    sum += grid.Get(cell.column + i, cell.row + j);
+                }
+            }
+            return sum;
         }
 
         private static IEnumerable<(int row, int column)> Cells()
@@ -55,6 +85,23 @@ namespace Spiral
                 }
 
                 yield return (row, column);
+            }
+        }
+
+        private class Grid
+        {
+            private const int dimension = 1000;
+            private const int offset = dimension / 2;
+            private readonly int[,] _grid = new int[dimension, dimension];
+
+            public void Set(int column, int row, int value)
+            {
+                _grid[row + offset, column + offset] = value;
+            }
+
+            public int Get(int column, int row)
+            {
+                return _grid[row + offset, column + offset];
             }
         }
     }
