50af52b4fad930c8c4e0440bbb4ad35697825a56
Jeroen Haegebaert <jeroen@haegebaert.com>
2017-12-09 11:10:41 +0100

    day 9 in C#

---
+++ day 09/Jeroen - C#/Garbage/Tests.cs
@@ -0,0 +1,69 @@
+﻿using Xunit;
+
+namespace Garbage
+{
+    public class Tests
+    {
+
+        [Theory]
+        [InlineData("<>", 0)]
+        [InlineData("<random characters>", 0)]
+        [InlineData("<<<<>", 0)]
+        [InlineData("<{!>}>", 0)]
+        [InlineData("<!!>", 0)]
+        [InlineData("<!!!>>", 0)]
+        [InlineData("<{o\"i!a,<{i<a>", 0)]
+        public void MostlyGarbage(string input, int expected)
+        {
+            var processor = new GarbageProcessor();
+            var result = processor.Process(input);
+            Assert.Equal(expected, result.Score);
+        }
+
+        [Theory]
+        [InlineData("<>", 0)]
+        [InlineData("<random characters>", 17)]
+        [InlineData("<<<<>", 3)]
+        [InlineData("<{!>}>", 2)]
+        [InlineData("<!!>", 0)]
+        [InlineData("<!!!>>", 0)]
+        [InlineData("<{o\"i!a,<{i<a>", 10)]
+        public void GarbageCount(string input, int expected)
+        {
+            var processor = new GarbageProcessor();
+            var result = processor.Process(input);
+            Assert.Equal(expected, result.GarbageCount);
+        }
+
+        [Theory]
+        [InlineData("{}", 1)]
+        [InlineData("{{{}}}", 3)]
+        [InlineData("{{},{}}", 3)]
+        [InlineData("{{{},{},{{}}}}", 6)]
+        [InlineData("{<{},{},{{}}>}", 1)]
+        [InlineData("{<a>,<a>,<a>,<a>}", 1)]
+        [InlineData("{{<a>},{<a>},{<a>},{<a>}}", 5)]
+        [InlineData("{{<!>},{<!>},{<!>},{<a>}}", 2)]
+        public void Groups(string input, int expected)
+        {
+            var processor = new GarbageProcessor();
+            var result = processor.Process(input);
+            Assert.Equal(expected, result.Groups);
+        }
+
+        [Theory]
+        [InlineData("{}", 1)]
+        [InlineData("{{{}}}", 6)]
+        [InlineData("{{},{}}", 5)]
+        [InlineData("{{{},{},{{}}}}", 16)]
+        [InlineData("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9)]
+        [InlineData("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9)]
+        [InlineData("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3)]
+        public void Score(string input, int expected)
+        {
+            var processor = new GarbageProcessor();
+            var result = processor.Process(input);
+            Assert.Equal(expected, result.Score);
+        }
+    }
+}
