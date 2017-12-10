700747477dc6bf806fa63edfb9c119b751b910b5
Gert Nelissen <Gert.Nelissen@ae.be>
2017-12-10 08:51:53 +0100

    Gert Day 10

---
+++ day 10/Runner.scala
@@ -0,0 +1,61 @@
+package main.scala.y2017.Day10
+
+object Runner extends Runner {}
+
+class Runner {
+  def run(inputs: List[Int], listSize: Int = 256): Int = {
+    val list = doRounds(inputs, listSize)
+    list(0) * list(1)
+  }
+
+  // Part 2
+  def run2(inputs: String, listSize: Int = 256): String = {
+    val inputSuffix = List(17, 31, 73, 47, 23)
+    val actualInputs = inputs.toCharArray.map(_.toInt).toList ::: inputSuffix
+
+    val mashedUpList = doRounds(actualInputs, listSize, 64)
+    createHash(mashedUpList)
+  }
+
+  def createHash(input: Array[Int]): String = {
+    input.grouped(16).map(_.reduce((a, b) => a ^ b))
+      .map(element => "%02x".format(element)).mkString("")
+  }
+
+  def doRounds(inputs: List[Int], listSize: Int, rounds: Int = 1): Array[Int] = {
+    var list = initList(listSize)
+    var position = 0
+    var skip = 0
+
+    (1 to rounds).foreach(round => {
+      inputs.foreach(input => {
+        list = rotate(list, position, input)
+        position = (position + input + skip) % list.length
+        skip = skip + 1
+      })
+    })
+
+    list
+  }
+
+  def initList(listSize: Int): Array[Int] = (0 until listSize).toArray
+
+  def rotate(initialList: Array[Int], from: Int, length: Int): Array[Int] = {
+    var list = initialList
+
+    (0 until length / 2).foreach(swapPosition => {
+      val position = (from + swapPosition) % list.length
+      val otherPosition = (from + length - 1 - swapPosition) % list.length
+      list = swap(list, position, otherPosition)
+    })
+
+    list
+  }
+
+  def swap(list: Array[Int], posA: Int, posB: Int): Array[Int] = {
+    val tmp = list(posB)
+    list.update(posB, list(posA))
+    list.update(posA, tmp)
+    list
+  }
+}
---
+++ day 10/Tests.scala
@@ -0,0 +1,26 @@
+package main.scala.y2017.Day10
+
+import main.scala.TestBase
+
+import scala.io.Source
+
+class Tests extends TestBase {
+  test("Example 1") { Runner.run(List(3, 4, 1, 5), 5) shouldBe 12 }
+
+  test("Puzzle 1") {
+    Runner.run(List(31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33)) shouldBe 6952
+  }
+
+  test("Creating hash") {
+    Runner.createHash(Array(65,27,9,1,4,3,40,50,91,7,6,0,2,5,68,22)) shouldBe "40"
+  }
+
+  test("Example 2.1") { Runner.run2("") shouldBe "a2582a3a0e66e6e86e3812dcb672a272" }
+  test("Example 2.2") { Runner.run2("AoC 2017") shouldBe "33efeb34ea91902bb2f59c9920caa6cd" }
+  test("Example 2.3") { Runner.run2("1,2,3") shouldBe "3efbe78a8d82f29979031a4aa0b16a9d" }
+  test("Example 2.4") { Runner.run2("1,2,4") shouldBe "63960835bcdc130f0b66d7ff4f6a5a8e" }
+
+  test("Puzzle 2") {
+    Runner.run2("31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33") shouldBe "28e7c4360520718a5dc811d3942cf1fd"
+  }
+}
