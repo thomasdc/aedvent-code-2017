874278351751919c988380806f1e412ff0741f10
ThomasDC <Thomas.DeCraemer@ravago.com>
2017-12-11 09:18:11 +0100

    day 11 part 1 c#

---
+++ day 11/Thomas - C#/Hex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Part1(File.ReadAllText("input.txt").Split(",")));
+        }
+
+        private static int Part1(string[] directions)
+        {
+            var x = 0;
+            var y = 0;
+            var z = 0;
+            foreach (var direction in directions)
+            {
+                (x, y, z) = Move(x, y, z, direction);
+            }
+
+            return new [] {Math.Abs(x), Math.Abs(y), Math.Abs(z)}.Max();
+        }
+
+        private static (int x, int y, int z) Move(int x, int y, int z, string direction)
+        {
+            if (direction == "n") return (x, y + 1, z - 1);
+            if (direction == "nw") return (x - 1, y + 1, z);
+            if (direction == "sw") return (x - 1, y, z + 1);
+            if (direction == "s") return (x, y - 1, z + 1);
+            if (direction == "se") return (x + 1, y - 1, z);
+            if (direction == "ne") return (x + 1, y, z - 1);
+            throw new Exception("I'm afraid I cannot do that Dave");
+        }
+    }
+}
