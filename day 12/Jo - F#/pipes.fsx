8dfd560fde7cd5d14eed2909eb0e24718cbcad99
Jo Van Eyck <jo.vaneyck@ae.be>
2017-12-12 12:36:17 +0100

    Day 12 part 1

--- day 12/Jo - F#/pipes.fsx
+++ day 12/Jo - F#/pipes.fsx
@@ -6,9 +6,9 @@ let split (delimiter : string) (text : string) =
     text.Split([|delimiter|], StringSplitOptions.RemoveEmptyEntries)
     |> List.ofSeq
 
-type Connection = int*int
+type DirectConnections = (int*int) list
 
-let parse input =
+let parse input : DirectConnections =
     let parseInt = Int32.Parse
     let parseLine line =
         let src :: destinations :: _ = split " <-> " line
@@ -20,8 +20,36 @@ let parse input =
     |> split "\n"
     |> List.collect parseLine
 
-let input = @"0 <-> 1,2
-1 <-> 2,3
-2 <-> 4"
+let directNeighbours location connections =
+    connections 
+    |> List.filter (fun (src, _) -> src = location)
+    |> List.map snd
 
-parse input
+let rec reachableFrom location connections = 
+    let neighbours = directNeighbours location connections
+    location :: neighbours
+
+let rec fixpoint f x =
+    let y = f x
+    if x = y 
+    then x
+    else fixpoint f y
+
+let expand (connections : DirectConnections) group =
+    let step = group |> List.collect (fun element -> reachableFrom element connections)
+    group @ step |> List.distinct
+
+let solve start connections = fixpoint (expand connections) [start]
+
+//let input = @"0 <-> 1,2
+//1 <-> 2,3
+//2 <-> 4
+//3 <-> 3
+//4 <-> 2,5
+//5 <-> 5"
+let input = System.IO.File.ReadAllText( __SOURCE_DIRECTORY__ + "\input.txt")
+
+input
+|> parse
+|> solve 0
+|> List.length
