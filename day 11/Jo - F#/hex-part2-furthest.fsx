dba0eecda9f2c456c6ca6476bb4499c210be2dbe
Jo Van Eyck <jo.vaneyck@ae.be>
2017-12-12 09:15:56 +0100

    fix typo in day 11

--- day 11/Jo - F#/hex-part2-furthest.fsx
+++ day 11/Jo - F#/hex-part2-furthest.fsx
@@ -27,7 +27,7 @@ let distance (x,y,z) =
     let abs (n : int) = System.Math.Abs n
     ([abs x ; abs y ; abs z] |> List.sum) / 2
 
-type State = { location : int * int * int; furthest : int }
+type State = { location : int * int * int; farthest : int }
 
 let run text =
     let parsed = 
@@ -35,7 +35,7 @@ let run text =
         |> split
         |> List.map parse
 
-    let takeStep {location = location; furthest = furthest} step =
+    let takeStep {location = location; farthest = farthest} step =
         let (x,y,z) = location
 
         let newLocation = 
@@ -47,13 +47,13 @@ let run text =
             | SW -> (x-1,y,z+1)
             | NW -> (x-1,y+1,z)
 
-        let newFurthest = [furthest; distance newLocation] |> List.max
+        let newFarthest = [farthest; distance newLocation] |> List.max
 
-        {location = newLocation; furthest = newFurthest}
+        {location = newLocation; farthest = newFarthest}
 
     let endLocation =
         parsed
-        |> List.fold takeStep { location = (0,0,0); furthest = 0 }
+        |> List.fold takeStep { location = (0,0,0); farthest = 0 }
     endLocation
 
 let input = System.IO.File.ReadAllText( __SOURCE_DIRECTORY__ + "\input.txt")
