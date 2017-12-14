7de2ff5ecab2931a3e643b28aba225c7eb574a46
Jeroen Haegebaert <jeroen@haegebaert.com>
2017-12-14 09:17:30 +0100

    day 13 in c#

---
+++ day 13/Jeroen - C#/firewall/Firewall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace firewall
+{
+    class Firewall
+    {
+        public static int Severity(IList<(int depth, int range)> input) 
+            => input.Select(x => Severity(x.depth, x.range, 0)).Sum();
+
+        public static int DelayToEscape(IList<(int depth, int range)> input)
+            => Enumerable.Range(0, int.MaxValue).First(delay => !input.Any(t => Caught(t.depth, t.range, delay)));
+
+        static int Severity(int depth, int range, int delay) 
+            => Caught(depth, range, delay) ? depth * range : 0;
+
+        static bool Caught(int depth, int range, int delay) 
+            => (depth + delay) % ((range - 1) * 2) == 0;
+    }
+}
---
+++ day 13/Jeroen - C#/firewall/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace firewall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items =(
+                from line in File.ReadLines("input.txt")
+                let indexes = line.Split(": ").Select(int.Parse).ToArray()
+                select (layer: indexes[0], range: indexes[1])
+                ).ToArray();
+
+            Run(() => Firewall.Severity(items));
+
+            Run(() => Firewall.DelayToEscape(items));
+
+        }
+
+        static void Run<T>(Func<T> f)
+        {
+            var sw = Stopwatch.StartNew();
+            var result = f();
+            Console.WriteLine($"{result} / {sw.Elapsed}");
+        }
+    }
+
+    static class Ex
+    {
+        public static IEnumerable<string> ReadLines(this TextReader input)
+        {
+            while (input.Peek() >= 0) yield return input.ReadLine();
+        }
+    }
+}
