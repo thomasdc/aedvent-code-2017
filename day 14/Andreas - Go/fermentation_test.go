d51ec191832b6d9f93d04c2d53f67fc41faea379
andreasDL <andreasdelille@hotmail.com>
2017-12-14 23:17:07 +0100

    part 2

--- day 14/Andreas - Go/fermentation_test.go
+++ day 14/Andreas - Go/fermentation_test.go
@@ -7,10 +7,16 @@ import (
 var tests = []struct{
 	input string
 	output int
+	regions int 
 }{
 	{
 		input: "flqrgnkx",
 		output: 8108,
+		regions: 1242,
+	},{
+		input: "hfdlxzhv",
+		output: 8230,
+		regions: 1103,
 	},
 }
 
@@ -29,3 +35,21 @@ func TestPart1(t *testing.T){
 		}
 	}
 }
+
+
+func TestPart2(t *testing.T){
+	
+	for _, test := range tests {
+
+		actual := Part2(test.input)
+		
+		if actual != test.regions {
+			t.Fatalf("Mistakes were made.. %s returned %d expecting %d.", 
+				test.input,
+				actual, 
+				test.regions,
+			)
+		}
+	}
+}
+
