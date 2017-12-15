0f972d1a46b9398837f6f9f5a7aa00bd5cae5d23
Jeroen Haegebaert <jeroen@haegebaert.com>
2017-12-15 12:37:47 +0100

    minor cleanup

--- day 15/Jeroen - C#/generator/Generator.cs
+++ day 15/Jeroen - C#/generator/Generator.cs
@@ -5,13 +5,17 @@ namespace generator
 {
     public static class Generator
     {
+        private const int FactorA = 16807;
+        private const int FactorB = 48271;
+        private const int Divisor = 2147483647;
+
         public static int GetNofMatches(int seedA, int seedB, int take, int multipleOfA = 1, int multipleOfB = 1) 
             => A(seedA, multipleOfA).Zip(B(seedB, multipleOfB), (a, b) => ((a: a, b: b)))
                 .Take(take)
                 .Count(x => (x.a & 0xFFFFL) == (x.b & 0xFFFFL));
 
-        public static IEnumerable<long> A(long seed, int multipleOf = 1) => Sequence(seed, 16807, 2147483647).Where(i => i % multipleOf == 0);
-        public static IEnumerable<long> B(long seed, int multipleOf = 1) => Sequence(seed, 48271, 2147483647).Where(i => i % multipleOf == 0);
+        public static IEnumerable<long> A(long seed, int multipleOf = 1) => Sequence(seed, FactorA, Divisor).Where(i => i % multipleOf == 0);
+        public static IEnumerable<long> B(long seed, int multipleOf = 1) => Sequence(seed, FactorB, Divisor).Where(i => i % multipleOf == 0);
         public static IEnumerable<long> Sequence(long input, long factor, long divisor)
         {
             var i = input;
