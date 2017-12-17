651777246c9e780017b169f72e294fa29729deee
Alex Wauters <alex.wauters@gmail.com>
2017-12-17 21:26:44 +0100

    Day 17 with rings in Go

--- day 17/Alex - Go/spinlock.go
+++ day 17/Alex - Go/spinlock.go
@@ -13,17 +13,18 @@ func main() {
 func part1() {
 	r := ring.New(1)
 	r.Value = 0
-	spinlock(r, 2017)
+	r = spinlock(r, 2017)
 	fmt.Println(r.Next().Value)
 }
 
-func spinlock(r *ring.Ring, iterations int) {
+func spinlock(r *ring.Ring, iterations int) *ring.Ring {
 	for i := 1; i <= iterations; i++ {
-		r = r.Move(301)
-		ins := &ring.Ring{Value: i}
+		r = r.Move(301 % i )
+		ins := &ring.Ring{Value:i}
 		r.Link(ins)
 		r = ins
 	}
+	return r
 }
 
 func part2() {
