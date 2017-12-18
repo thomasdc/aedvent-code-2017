8451d7025b3a3bbe998c550b01a13845c9cac58a
andreasDL <andreasdelille@hotmail.com>
2017-12-18 22:18:57 +0100

    part2 - not 100pct clean

--- day 18/Andreas - Go/duet2.go
+++ day 18/Andreas - Go/duet2.go
@@ -8,13 +8,22 @@ import (
 )
 
 type prog struct{
+	id int
 	registers map[string]int
-	lastValue int	
+	in  <-chan int //input from other prog
+	out chan<- int //output to other prog
+	sch chan<- int //send counter to main thread
+	ctr int
 }
-
-//register read & writes
-func (p *prog) snd(x string){ 
-	p.lastValue = p.registers[x] 
+func NewProg(id int, in <-chan int, out chan<- int, sch chan<- int) *prog{
+	return &prog{
+		id		 : id,
+		registers: map[string]int{"p": id}, 
+		in: in,
+		out: out,
+		sch: sch,
+		ctr: 0,
+	}
 }
 func (p *prog) set(x string, y string){
 	if val, err := strconv.Atoi(y) ; err == nil {
@@ -44,50 +53,64 @@ func (p *prog) mod(x string, y string){
 		p.registers[x] %= p.registers[y]
 	}
 }
-func (p *prog) jgz(x string)int { //reg value or value on its own ?
+func (p *prog) jgz(x string)int { //abuse this function => reg value or value on its own ?
 	if val, err := strconv.Atoi(x) ; err == nil {
 		return val
 	} else {
 		return p.registers[x]
 	}
 }
-
-func NewProg() *prog{
-	return &prog{map[string]int{}, 0}
+func (p *prog) snd(x string){
+	p.out <- p.jgz(x)
+	p.ctr++
+	p.sch <- p.ctr
 }
+func (p *prog) rcv(x string){
+	p.registers[x] = <- p.in
+}
+func (p *prog) run(instructions []string){
 
-func main() {
-	//input := readInput("testInput.txt")
-	input := readInput("input.txt")
-	
-	instructions := *parseInstructions(input)
 	pos := 0
-	prog := *NewProg()
 	for pos < len(instructions) && pos >= 0 {
 
 		instr := strings.Split(instructions[pos], " ")
-
 		switch instr[0] {
-			case "snd": prog.snd(instr[1])
-			case "set": prog.set(instr[1],instr[2])
-			case "add": prog.add(instr[1],instr[2])
-			case "mul": prog.mul(instr[1],instr[2])
-			case "mod": prog.mod(instr[1],instr[2])
-			case "rcv": 
-				if prog.jgz(instr[1]) != 0 {
-					fmt.Println(prog.lastValue)
-					panic(nil) //fuckit lets quit !
-				}
+			case "set": p.set(instr[1],instr[2])
+			case "add": p.add(instr[1],instr[2])
+			case "mul": p.mul(instr[1],instr[2])
+			case "mod": p.mod(instr[1],instr[2])
+			case "snd": p.snd(instr[1])
+			case "rcv": p.rcv(instr[1])
 			case "jgz":
-				if prog.jgz(instr[1]) > 0 {
-					pos += prog.jgz(instr[2]) - 1 //pos++ comes later => -1
+				if p.jgz(instr[1]) > 0 {
+					pos += p.jgz(instr[2]) - 1 //pos++ comes later => -1
 				}
 		}
-		
 		pos++
+		//fmt.Println(p.id, instr, p.registers, pos)
 	}
+	
+	close(p.out)
 }
 
+
+
+func main() {
+	//input := readInput("testInput2.txt")
+	input := readInput("input.txt")
+	instructions := *parseInstructions(input)
+	
+	ch0to1, ch1to0 := make(chan int, 1000), make(chan int, 1000)
+	snd0, snd1 := make(chan int), make(chan int)
+
+	prog0 := NewProg(0, ch1to0, ch0to1, snd0)
+	prog1 := NewProg(1, ch0to1, ch1to0, snd1)
+
+	go prog0.run(instructions)
+	go prog1.run(instructions)
+
+	for { fmt.Println(<-snd0, " <-> ", <-snd1) }
+}
 
 func parseInstructions(input string) *[]string{
 	res := strings.Split(input, "\n")
