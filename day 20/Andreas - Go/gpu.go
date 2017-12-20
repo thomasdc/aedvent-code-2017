ecf79b2e68e51912e3eacd54aabbaab7905bd171
andreasDL <andreasdelille@hotmail.com>
2017-12-20 20:15:08 +0100

    tomap

--- day 20/Andreas - Go/gpu.go
+++ day 20/Andreas - Go/gpu.go
@@ -36,11 +36,10 @@ func main() {
 	input := readInput("input.txt")
 	particles := *(parseInput(input))
 
-	//for i, p := range particles { fmt.Println(i, " => ", p, p.distanceOrigin(), p.accelerationOrigin()) }
 
 	min := 0
 	for i, p := range particles {
-		if particles[min].accelerationOrigin() > p.accelerationOrigin() { min = i }
+		if (*particles[min]).accelerationOrigin() > (*p).accelerationOrigin() { min = i }
 	}
 	fmt.Println(min)
 }
@@ -49,15 +48,16 @@ func readInput(fname string) string {
 	s, _ := ioutil.ReadFile(fname)
 	return string(s)
 }
-func parseInput(s string) *[]particle{
-	lines := strings.Split(s, "\n")
-	res := make([]particle, len(lines))
-	for i, line := range lines {
+func parseInput(s string) *map[int]*particle{
+	
+	res := map[int]*particle{}
+	for i, line := range strings.Split(s, "\n") {
+
 		line = line[:len(line)-1]	
 		parts := strings.Split(line, ">, ")
 		p, v, a := parsePart(parts[0]), parsePart(parts[1]), parsePart(parts[2])
 
-		res[i] = particle{
+		res[i] = &particle{
 			p[0], p[1], p[2],
 			v[0], v[1], v[2],
 			a[0], a[1], a[2],
