e69820ba45aadc5a6c1f3a03d7de093a8644d68c
ThomasDC <Thomas.DeCraemer@ravago.com>
2017-12-22 14:00:21 +0100

    day 15 part 2 c#

--- day 15/Thomas - C#/Generator/Program.cs
+++ day 15/Thomas - C#/Generator/Program.cs
@@ -8,7 +8,7 @@ namespace Generator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Part1(703, 516));
+            Console.WriteLine(Part2(703, 516));
         }
 
         private static int Part1(int input1, int input2)
@@ -16,14 +16,30 @@ namespace Generator
             const int numberOfIterations = 40_000_000;
             return Iterate(input1, 16807).Take(numberOfIterations).Zip(Iterate(input2, 48271).Take(numberOfIterations), (a, b) => BitsMatch(a, b) ? 1 : 0).Sum();
         }
-
-        private static IEnumerable<long> Iterate(long start, int multiplicationFactor)
+        
+        private static int Part2(int input1, int input2)
+        {
+            const int numberOfIterations = 5_000_000;
+            return Iterate(input1, 16807, 2).Take(numberOfIterations).Zip(Iterate(input2, 48271, 3).Take(numberOfIterations), (a, b) => BitsMatch(a, b) ? 1 : 0).Sum();
+        }
+        
+        private static IEnumerable<long> Iterate(long start, int multiplicationFactor, int bitShift = 0)
         {
             var next = start;
             while (true)
             {
                 next = (next * multiplicationFactor) % int.MaxValue;
-                yield return next;
+                if (bitShift > 0)
+                {
+                    if (next << (64 - bitShift) == 0)
+                    {
+                        yield return next;
+                    }
+                }
+                else
+                {
+                    yield return next;
+                }
             }
         }
 
