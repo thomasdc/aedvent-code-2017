728ff20575b122f917eb76da90be4c74f6b93540
andreasDL <andreasdelille@hotmail.com>
2017-12-22 21:24:51 +0100

    part2

--- day 22/Andreas - Go/sporifica2.go
+++ day 22/Andreas - Go/sporifica2.go
@@ -24,82 +24,98 @@ var example = `..#
 #..
 ...`
 
+type health int
+const (
+	C health = iota
+	W
+	I
+	F
+)
+
+
 type Field struct {
-	nodes map[int]map[int]bool //nodes[y][x] = isInfected()
+	nodes map[int]map[int]health //nodes[y][x] = isInfected()
 	x,y int
 	dir direction
 	ctr int
 }
+
 
 func main() {
 		
 	//example
 	field := NewField()
 	field.loadMap(example)
-
-	fmt.Println(field) 
-	field.print()
-		
-	for i := 0 ; i < 10000 ; i++ {
-		field.step()
-	}
-	//field.print()
+	for i := 0 ; i < 10000000 ; i++ { field.step() }
 	fmt.Println(field.ctr)
 
-	//part1
+
+	//part2
 	field = NewField()
 	field.loadMap( readInput("input.txt") )
-	for i := 0; i < 10000; i++ { field.step() }
+	for i := 0 ; i < 10000000 ; i++ { field.step() }
 	fmt.Println(field.ctr)
-
-
 }
 
 func NewField() *Field{
 	return &Field{
-		nodes: map[int]map[int]bool{},
+		nodes: map[int]map[int]health{},
 		x: 0,
 		y: 0,
 		dir: U,
 	}
 }
 func (f *Field) step(){
-	if f.nodes[f.y][f.x] {
-		f.dir = (f.dir+1)%4
-	} else {
-		f.dir = (f.dir+3)%4
+	switch f.nodes[f.y][f.x] {
+		case C: f.dir = (f.dir+3)%4
+		case I: f.dir = (f.dir+1)%4
+		case F: f.dir = (f.dir+2)%4
 	}
 
-	if _, ex := f.nodes[f.y] ; !ex { f.nodes[f.y] = map[int]bool{} }
-	f.nodes[f.y][f.x] = !f.nodes[f.y][f.x]
-	if f.nodes[f.y][f.x] { f.ctr++ }
+	//lookup fix !
+	if _, ex := f.nodes[f.y] ; !ex { f.nodes[f.y] = map[int]health{} }
+		
+	//new state
+	f.nodes[f.y][f.x] = (f.nodes[f.y][f.x]+1)%4
+	if f.nodes[f.y][f.x] == I { 
+		f.ctr++ 
+	}
 
 	f.x += dxdy[f.dir].dx
 	f.y += dxdy[f.dir].dy
 }
 func (f *Field) loadMap(s string){
-	f.nodes = map[int]map[int]bool{}
+	f.nodes = map[int]map[int]health{}
 	
 	lines := strings.Split(s, "\n")
 	offset := int(len(lines)/2)
 
 	for j, y := 0, offset ; y >= -offset ; j,y = j+1, y-1 {
 		line := lines[j]
-		if _, ex := f.nodes[y] ; !ex { f.nodes[y] = map[int]bool{} }
+		if _, ex := f.nodes[y] ; !ex { f.nodes[y] = map[int]health{} }
 		
 		for i,x := 0, -offset ; x <= offset ; i,x = i+1, x+1 {
-			f.nodes[y][x] = line[i] == '#'
+			switch line[i]{
+				case '.': f.nodes[y][x] = C
+				case '#': f.nodes[y][x] = I
+				case 'W': f.nodes[y][x] = W
+				case 'F': f.nodes[y][x] = F
+			}
 		}
 	}
 }
 func (f *Field) print(){
 	r := int(len(f.nodes)/2) + 1
-
 	for i := r ; i >= -r ; i-- {
 		for j := -r ; j <= r ; j++ {
 
-			if infected, ex := f.nodes[i][j] ; ex && infected {
-				fmt.Print("#")
+			if h, ex := f.nodes[i][j] ; ex {
+				switch h {
+					case C: fmt.Print(".")
+					case W: fmt.Print("W")
+					case F: fmt.Print("F")
+					case I: fmt.Print("#")
+				}
 			} else {
 				fmt.Print(".")
 			}
