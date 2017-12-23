ae307f5fbd3defb1e147b830b038e4190a03e4c0
ThomasDC <Thomas.DeCraemer@ravago.com>
2017-12-23 18:09:01 +0100

    day 17 part 1 c#

---
+++ day 17/Thomas - C#/Spinlock/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spinlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Part1(359));
+        }
+
+        private static int Part1(int stepsize)
+        {
+            var list = new List<int> {0};
+            var location = 0;
+            
+            for (var i = 1; i <= 2017; i++)
+            {
+                location = (location + stepsize) % list.Count + 1;
+                list.Insert(location, i);
+            }
+
+            return list[list.IndexOf(2017) + 1];
+        }
+    }
+}
