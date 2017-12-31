363a3e8e64f64bcce75c2f5e7fcbb5ea2b255f6e
ThomasDC <de.craemer.thomas@gmail.com>
2017-12-31 12:13:27 +0100

    day 18 part 2 c#

--- day 18/Thomas - C#/Duet/Program.cs
+++ day 18/Thomas - C#/Duet/Program.cs
@@ -9,32 +9,28 @@ namespace Duet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Part1());
+            Console.WriteLine(Part2());
             Console.ReadLine();
         }
 
-        private static long Part1()
+        private static long Part2()
         {
-            var instructions = Parse(File.ReadAllLines("input.txt"));
-            var registers = new Dictionary<string, long>();
-            foreach (var @char in "abcdefghijklmnop".Select(_ => _.ToString()))
+            var program0 = new Programma(0);
+            program0.AssignInstructions(Parse(File.ReadAllLines("input.txt"), program0));
+            var program1 = new Programma(1);
+            program1.AssignInstructions(Parse(File.ReadAllLines("input.txt"), program1));
+            program0.SetOtherProgram(program1);
+
+            while (!(program0.BusyWaiting && program1.BusyWaiting))
             {
-                registers[@char] = 0;
+                program0.Tick();
+                program1.Tick();
             }
 
-            var currentInstructionIndex = 0;
-            var @continue = true;
-            while (@continue)
-            {
-                var applied = instructions[currentInstructionIndex].Apply(registers, currentInstructionIndex);
-                currentInstructionIndex = applied.nextInstructionIndex;
-                @continue = applied.@continue;
-            }
-
-            return Sound.LastFrequencyPlayed;
+            return program1.SendCount;
         }
 
-        private static Instruction[] Parse(string[] inputLines)
+        private static Instruction[] Parse(string[] inputLines, Programma program)
         {
             var instructions = new List<Instruction>();
             foreach (var line in inputLines)
@@ -42,12 +38,12 @@ namespace Duet
                 var split = line.Split(' ');
                 switch (split[0])
                 {
-                    case "snd": instructions.Add(new Sound(split[1])); break;
+                    case "snd": instructions.Add(new Send(split[1], program)); break;
                     case "set": instructions.Add(new Set(split[1], split[2])); break;
                     case "add": instructions.Add(new Add(split[1], split[2])); break;
                     case "mul": instructions.Add(new Multiply(split[1], split[2])); break;
                     case "mod": instructions.Add(new Mod(split[1], split[2])); break;
-                    case "rcv": instructions.Add(new Recover(split[1])); break;
+                    case "rcv": instructions.Add(new Receive(split[1], program)); break;
                     case "jgz": instructions.Add(new Jump(split[1], split[2])); break;
                 }
             }
@@ -55,25 +51,87 @@ namespace Duet
             return instructions.ToArray();
         }
 
-        public interface Instruction
+        class Programma
         {
-            (int nextInstructionIndex, bool @continue) Apply(IDictionary<string, long> registers, int currentInstructionIndex);
-        }
+            public int Id;
+            private Programma _otherProgram;
+            private readonly Dictionary<string, long> _registers;
+            private readonly Queue<long> _mailBox;
+            public int SendCount;
+            public bool BusyWaiting;
+            private int _currentInstructionIndex;
+            private Instruction[] _instructions;
 
-        class Sound : Instruction
-        {
-            private readonly string _x;
-            public static long LastFrequencyPlayed;
-
-            public Sound(string x)
+            public Programma(int id)
             {
-                _x = x;
+                Id = id;
+                _mailBox = new Queue<long>();
+                BusyWaiting = false;
+
+                _registers = new Dictionary<string, long>();
+                foreach (var @char in "abcdefghijklmnop".Select(_ => _.ToString()))
+                {
+                    _registers[@char] = 0;
+                }
+
+                _registers["p"] = id;
             }
 
-            public (int nextInstructionIndex, bool @continue) Apply(IDictionary<string, long> registers, int currentInstructionIndex)
+            public void AssignInstructions(Instruction[] instructions)
             {
-                LastFrequencyPlayed = registers[_x];
-                return (currentInstructionIndex + 1, true);
+                _instructions = instructions;
+            }
+
+            public void Tick()
+            {
+                _currentInstructionIndex = _instructions[_currentInstructionIndex].Apply(_registers, _currentInstructionIndex);
+            }
+
+            public void Send(long value)
+            {
+                SendCount++;
+                _otherProgram._mailBox.Enqueue(value);
+            }
+
+            public bool TryReceive(out long result)
+            {
+                if (_mailBox.TryDequeue(out result))
+                {
+                    BusyWaiting = false;
+                    return true;
+                }
+
+                BusyWaiting = true;
+                return false;
+            }
+
+            public void SetOtherProgram(Programma other)
+            {
+                _otherProgram = other;
+                _otherProgram._otherProgram = this;
+            }
+        }
+
+        public interface Instruction
+        {
+            int Apply(IDictionary<string, long> registers, int currentInstructionIndex);
+        }
+
+        class Send : Instruction
+        {
+            private readonly Func<IDictionary<string, long>, long> _xSelector;
+            private readonly Programma _program;
+
+            public Send(string x, Programma program)
+            {
+                _xSelector = AsNumberOrRegisterValue(x);
+                _program = program;
+            }
+
+            public int Apply(IDictionary<string, long> registers, int currentInstructionIndex)
+            {
+                _program.Send(_xSelector(registers));
+                return currentInstructionIndex + 1;
             }
         }
 
@@ -88,10 +146,10 @@ namespace Duet
                 _ySelector = AsNumberOrRegisterValue(y);
             }
 
-            public (int nextInstructionIndex, bool @continue) Apply(IDictionary<string, long> registers, int currentInstructionIndex)
+            public int Apply(IDictionary<string, long> registers, int currentInstructionIndex)
             {
                 registers[_x] = _ySelector(registers);
-                return (currentInstructionIndex + 1, true);
+                return currentInstructionIndex + 1;
             }
         }
 
@@ -106,10 +164,10 @@ namespace Duet
                 _ySelector = AsNumberOrRegisterValue(y);
             }
 
-            public (int nextInstructionIndex, bool @continue) Apply(IDictionary<string, long> registers, int currentInstructionIndex)
+            public int Apply(IDictionary<string, long> registers, int currentInstructionIndex)
             {
                 registers[_x] += _ySelector(registers);
-                return (currentInstructionIndex + 1, true);
+                return currentInstructionIndex + 1;
             }
         }
 
@@ -124,10 +182,10 @@ namespace Duet
                 _ySelector = AsNumberOrRegisterValue(y);
             }
 
-            public (int nextInstructionIndex, bool @continue) Apply(IDictionary<string, long> registers, int currentInstructionIndex)
+            public int Apply(IDictionary<string, long> registers, int currentInstructionIndex)
             {
                 registers[_x] %= _ySelector(registers);
-                return (currentInstructionIndex + 1, true);
+                return currentInstructionIndex + 1;
             }
         }
 
@@ -142,25 +200,33 @@ namespace Duet
                 _ySelector = AsNumberOrRegisterValue(y);
             }
 
-            public (int nextInstructionIndex, bool @continue) Apply(IDictionary<string, long> registers, int currentInstructionIndex)
+            public int Apply(IDictionary<string, long> registers, int currentInstructionIndex)
             {
                 registers[_x] *= _ySelector(registers);
-                return (currentInstructionIndex + 1, true);
+                return currentInstructionIndex + 1;
             }
         }
 
-        class Recover : Instruction
+        class Receive : Instruction
         {
             private readonly string _x;
+            private readonly Programma _program;
 
-            public Recover(string x)
+            public Receive(string x, Programma program)
             {
                 _x = x;
+                _program = program;
             }
 
-            public (int nextInstructionIndex, bool @continue) Apply(IDictionary<string, long> registers, int currentInstructionIndex)
+            public int Apply(IDictionary<string, long> registers, int currentInstructionIndex)
             {
-                return (currentInstructionIndex, false);
+                if (_program.TryReceive(out var receivedValue))
+                {
+                    registers[_x] = receivedValue;
+                    return currentInstructionIndex + 1;
+                }
+
+                return currentInstructionIndex;
             }
         }
 
@@ -175,14 +241,14 @@ namespace Duet
                 _ySelector = AsNumberOrRegisterValue(y);
             }
 
-            public (int nextInstructionIndex, bool @continue) Apply(IDictionary<string, long> registers, int currentInstructionIndex)
+            public int Apply(IDictionary<string, long> registers, int currentInstructionIndex)
             {
                 if (_xSelector(registers) > 0)
                 {
-                    return ((int) (currentInstructionIndex + _ySelector(registers)), true);
+                    return (int) (currentInstructionIndex + _ySelector(registers));
                 }
 
-                return (currentInstructionIndex + 1, true);
+                return currentInstructionIndex + 1;
             }
         }
 
