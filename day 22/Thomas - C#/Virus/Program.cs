8c8663ae08587461dfa8550a9238956e5543ac8d
ThomasDC <de.craemer.thomas@gmail.com>
2017-12-31 10:38:15 +0100

    day 22 part 2 c#

--- day 22/Thomas - C#/Virus/Program.cs
+++ day 22/Thomas - C#/Virus/Program.cs
@@ -9,14 +9,14 @@ namespace Virus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Part1(Parse(File.ReadAllLines("input.txt"))));
+            Console.WriteLine(Part2(Parse(File.ReadAllLines("input.txt"))));
             Console.ReadLine();
         }
 
-        private static int Part1(IDictionary<(int, int), bool> grid)
+        private static int Part2(IDictionary<(int, int), State> grid)
         {
             var carrier = new Carrier();
-            foreach (var iteration in Enumerable.Range(0, 10000))
+            foreach (var iteration in Enumerable.Range(0, 10000000))
             {
                 carrier.Burst(grid);
             }
@@ -24,10 +24,10 @@ namespace Virus
             return carrier.NumberOfInfections;
         }
 
-        private static IDictionary<(int, int), bool> Parse(string[] inputLines)
+        private static IDictionary<(int, int), State> Parse(string[] inputLines)
         {
             var gridSize = inputLines.First().Length;
-            var grid = new Dictionary<(int, int), bool>();
+            var grid = new Dictionary<(int, int), State>();
 
             for (var y = gridSize / 2; y >= -gridSize / 2; y--)
             {
@@ -35,7 +35,7 @@ namespace Virus
                 for (var x = -gridSize / 2; x <= gridSize / 2; x++)
                 {
                     var @char = line[x + gridSize / 2];
-                    grid[(x, y)] = @char == '#';
+                    grid[(x, y)] = @char == '#' ? State.Infected : State.Clean;
                 }
             }
 
@@ -48,20 +48,26 @@ namespace Virus
             private (int x, int y) _location = (0, 0);
             public int NumberOfInfections;
             
-            public void Burst(IDictionary<(int, int), bool> grid)
+            public void Burst(IDictionary<(int, int), State> grid)
             {
                 if (!grid.ContainsKey(_location))
                 {
-                    grid[_location] = false;
+                    grid[_location] = State.Clean;
                 }
 
-                Turn(!grid[_location]);
-                if (!grid[_location])
+                switch (grid[_location])
+                {
+                    case State.Clean: Turn(true); break;
+                    case State.Infected: Turn(false); break;
+                    case State.Flagged: Reverse(); break;
+                }
+                
+                if (grid[_location] == State.Weakened)
                 {
                     NumberOfInfections++;
                 }
 
-                grid[_location] = !grid[_location];
+                grid[_location] = (State) (((int) grid[_location] + 1) % 4);
                 MoveForward();
             }
 
@@ -82,11 +88,21 @@ namespace Virus
                     ? (Direction) (((int) _direction - 1 + 4) % 4)
                     : (Direction) (((int) _direction + 1) % 4);
             }
+
+            private void Reverse()
+            {
+                _direction = (Direction) (((int) _direction + 2) % 4);
+            }
         }
 
         enum Direction
         {
             Up = 0, Right = 1, Down = 2, Left = 3
+        }
+
+        enum State
+        {
+            Clean = 0, Weakened = 1, Infected = 2, Flagged = 3
         }
     }
 }
